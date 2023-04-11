8cbfcb16bc81c7f069ef28f91bd126fc7176181d
souben <sbannoun@gmail.com>
2023-04-10 23:19:25 -0500

    add splash avtivity

---
+++ app/src/main/java/com/angeltech/goo/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.angeltech.goo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        GlobalScope.launch{
+            delay(2000)
+            val intent = Intent(this@SplashActivity, MainActivity::class.java);
+            startActivity(intent)
+            finish()
+        }
+    }
+}
