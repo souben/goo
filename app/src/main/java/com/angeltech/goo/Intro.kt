3678fe2b8861de3611ebb68b04e3701bd5845f79
souben <sbannoun@gmail.com>
2023-04-11 21:58:40 -0500

    add data binding to project

---
+++ app/src/main/java/com/angeltech/goo/Intro.kt
@@ -0,0 +1,6 @@
+package com.angeltech.goo
+
+data class Intro(
+    var title : String = "",
+    var description : String = ""
+)
--- app/src/main/java/com/angeltech/goo/MainActivity.kt
+++ app/src/main/java/com/angeltech/goo/MainActivity.kt
@@ -1,43 +1,38 @@
 package com.angeltech.goo
 
 import android.content.Context
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import androidx.databinding.DataBindingUtil
+import com.angeltech.goo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var helloInput : EditText
-    lateinit var helloText : TextView
-    lateinit var doneButton : Button
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        doneButton = findViewById<Button>(R.id.doneButton)
-        helloText = findViewById<TextView>(R.id.helloText)
-        helloInput = findViewById<EditText>(R.id.helloInput)
-
-        doneButton.setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.doneButton.setOnClickListener {
             getUsername(it)
         }
 
+        var intro = Intro("Job", "Software engineer")
+        binding.intro = intro
     }
 
     private fun getUsername(view : View) {
-        val username = helloInput.text.toString()
-        if (username != "" ){
-            helloInput.visibility = View.GONE
-            helloText.text = username
-            helloText.visibility = View.VISIBLE
+        val username = binding.helloInput.text
+        if (username.toString() != "" ) {
+            binding.apply {
+                invalidateAll()
+                helloInput.visibility = View.GONE
+                helloText.text = username
+                helloText.visibility = View.VISIBLE
+            }
         }
+
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
